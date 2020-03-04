274c5f85bf92ea9b6b40c71200ecf482ca92a729
Bharath Srinivas <cybermaster192@gmail.com>
2020-03-04 21:47:08 +0530

    added predefined delays for the charsets

--- charsets.go
+++ charsets.go
@@ -1,5 +1,7 @@
 package termloader
 
+import "time"
+
 // Character set type
 type Charset []string
 
@@ -72,4 +74,78 @@ var Charsets = map[string]Charset{
 	"point":               {"∙∙∙", "●∙∙", "∙●∙", "∙∙●", "∙∙∙"},
 	"layer":               {"-", "=", "≡"},
 	"betaWave":            {"ρββββββ", "βρβββββ", "ββρββββ", "βββρβββ", "ββββρββ", "βββββρβ", "ββββββρ"},
+}
+
+// Delay type
+type Delay time.Duration
+
+// Predefined delays for the charsets.
+var Delays = map[string]time.Duration{
+	"default":             100 * time.Millisecond,
+	"dots":                80 * time.Millisecond,
+	"dots2":               80 * time.Millisecond,
+	"dots3":               80 * time.Millisecond,
+	"dots4":               80 * time.Millisecond,
+	"dots5":               80 * time.Millisecond,
+	"dots6":               80 * time.Millisecond,
+	"dots7":               80 * time.Millisecond,
+	"dots8":               80 * time.Millisecond,
+	"dots9":               80 * time.Millisecond,
+	"dots10":              80 * time.Millisecond,
+	"dots11":              100 * time.Millisecond,
+	"dots12":              80 * time.Millisecond,
+	"line":                130 * time.Millisecond,
+	"line2":               100 * time.Millisecond,
+	"line3":               100 * time.Millisecond,
+	"pipe":                100 * time.Millisecond,
+	"simpleDots":          400 * time.Millisecond,
+	"simpleDotsScrolling": 200 * time.Millisecond,
+	"star":                70 * time.Millisecond,
+	"star2":               80 * time.Millisecond,
+	"star3":               100 * time.Millisecond,
+	"flip":                70 * time.Millisecond,
+	"hamburger":           100 * time.Millisecond,
+	"growVertical":        120 * time.Millisecond,
+	"growHorizontal":      120 * time.Millisecond,
+	"balloon":             120 * time.Millisecond,
+	"balloon2":            120 * time.Millisecond,
+	"noise":               100 * time.Millisecond,
+	"bounce":              120 * time.Millisecond,
+	"boxBounce":           120 * time.Millisecond,
+	"boxBounce2":          100 * time.Millisecond,
+	"triangle":            50 * time.Millisecond,
+	"arc":                 100 * time.Millisecond,
+	"circle":              120 * time.Millisecond,
+	"squareCorners":       180 * time.Millisecond,
+	"circleQuarters":      120 * time.Millisecond,
+	"circleHalves":        50 * time.Millisecond,
+	"squish":              100 * time.Millisecond,
+	"toggle":              250 * time.Millisecond,
+	"toggle2":             80 * time.Millisecond,
+	"toggle3":             120 * time.Millisecond,
+	"toggle4":             100 * time.Millisecond,
+	"toggle5":             100 * time.Millisecond,
+	"toggle6":             300 * time.Millisecond,
+	"toggle7":             80 * time.Millisecond,
+	"toggle8":             100 * time.Millisecond,
+	"toggle9":             100 * time.Millisecond,
+	"toggle10":            100 * time.Millisecond,
+	"toggle11":            50 * time.Millisecond,
+	"toggle12":            120 * time.Millisecond,
+	"toggle13":            80 * time.Millisecond,
+	"arrow":               100 * time.Millisecond,
+	"arrow2":              80 * time.Millisecond,
+	"arrow3":              120 * time.Millisecond,
+	"arrow4":              100 * time.Millisecond,
+	"bouncingBar":         80 * time.Millisecond,
+	"bouncingBall":        80 * time.Millisecond,
+	"smiley":              200 * time.Millisecond,
+	"moon":                80 * time.Millisecond,
+	"pong":                80 * time.Millisecond,
+	"shark":               120 * time.Millisecond,
+	"dqpb":                100 * time.Millisecond,
+	"grenade":             80 * time.Millisecond,
+	"point":               125 * time.Millisecond,
+	"layer":               150 * time.Millisecond,
+	"betaWave":            80 * time.Millisecond,
 }
